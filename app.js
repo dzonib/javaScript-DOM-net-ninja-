a5a87c07d6ab41aadee7f136e77be3669283118f
dzonib <nikolabosnjak381@gmail.com>
2017-10-19 12:27:35 +0200

    how do properly delete books! Last method has issues with adding new books (cant delete new ones)

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
-/*const h2 = document.querySelector('#book-list h2');
+//better model, works with added book titles.
 
-h2.addEventListener('click', (e) => {
-  console.log(e.target);
-  console.log(e);
+const list = document.querySelector('#book-list ul');
+
+list.addEventListener('click', (e) => {
+  li = e.target.parentNode;
+  li.parentNode.removeChild(li);
 });
-Simple example of adding events*/
 
-
-const btns = document.querySelectorAll('#book-list .delete');
-
-btns.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    const li = e.target.parentNode;
-    li.parentNode.removeChild(li);
-  });
-});
-//more complicated example of events.
-//its deleting books from list.
-
-const preventDefault = document.querySelector('#page-banner a');
-
-preventDefault.addEventListener('click', (e) => {
-  e.preventDefault();
-  console.log('Navigation to ' , e.target.textContent, ' is prevented for safety reasons!');
-});
-//Prevents default behaviour! (link is disabled)
+//This is good way to delete books.
